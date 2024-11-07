ca29fa1e177d3a3190e206b0b86bd8b722924cdf
Evan Almloff <evanalmloff@gmail.com>
2024-11-07 14:39:45 -0600

    remove union from intermediate enum repr

--- tests/enum.rs
+++ tests/enum.rs
@@ -4,47 +4,47 @@ use std::mem::MaybeUninit;
 fn test_transmute_bytes_to_enum() {
     #[derive(Clone, Copy, Debug, PartialEq)]
     #[repr(C, u8)]
-    enum Enum {
+    enum Enum<T> {
         A { one: u32, two: u16 },
-        B { one: u8, two: u16 },
+        B { one: u8, two: T },
     }
 
-    #[repr(C)]
-    #[derive(Clone, Copy)]
-    struct CRepr {
-        tag: u8,
-        data: Merged,
-    }
+    // #[repr(C)]
+    // #[derive(Clone, Copy)]
+    // struct CRepr<T: Copy> {
+    //     tag: u8,
+    //     data: Merged<T>,
+    // }
+
+    // #[repr(C)]
+    // #[derive(Clone, Copy)]
+    // union Merged<T: Copy> {
+    //     a: A,
+    //     b: B<T>,
+    // }
 
     #[repr(C)]
-    #[derive(Clone, Copy)]
-    union Merged {
-        a: A,
-        b: B,
-    }
-
-    #[repr(C)]
-    #[derive(Clone, Copy, Debug, PartialEq)]
+    #[derive(Debug, PartialEq)]
     struct A {
         one: u32,
         two: u16,
     }
 
     #[repr(C)]
-    #[derive(Clone, Copy, Debug, PartialEq)]
-    struct B {
+    #[derive(Debug, PartialEq)]
+    struct B<T> {
         one: u8,
-        two: u16,
+        two: T,
     }
 
-    const SIZE: usize = std::mem::size_of::<Enum>();
+    const SIZE: usize = std::mem::size_of::<Enum<u16>>();
     let mut out = [MaybeUninit::uninit(); SIZE];
-    let tag_align = std::mem::offset_of!(CRepr, tag);
-    let data_align = std::mem::offset_of!(CRepr, data);
+    let tag_align = 0;
+    let data_align = 1.max(std::mem::align_of::<A>()).max(std::mem::align_of::<B<u16>>()); 
     let a_one_align = std::mem::offset_of!(A, one);
     let a_two_align = std::mem::offset_of!(A, two);
-    let b_one_align = std::mem::offset_of!(B, one);
-    let b_two_align = std::mem::offset_of!(B, two);
+    let b_one_align = std::mem::offset_of!(B<u16>, one);
+    let b_two_align = std::mem::offset_of!(B<u16>, two);
 
     let one = 1234u32;
     let two = 5678u16;
@@ -59,7 +59,7 @@ fn test_transmute_bytes_to_enum() {
         out[data_align + i + a_two_align] = MaybeUninit::new(*byte);
     }
     out[tag_align] = MaybeUninit::new(0);
-    let out = unsafe { std::mem::transmute_copy::<[MaybeUninit<u8>; SIZE], Enum>(&out) };
+    let out = unsafe { std::mem::transmute_copy::<[MaybeUninit<u8>; SIZE], Enum<u16>>(&out) };
     assert_eq!(out, first);
 
     let mut out = [MaybeUninit::uninit(); SIZE];
@@ -76,6 +76,6 @@ fn test_transmute_bytes_to_enum() {
         out[data_align + i + b_two_align] = MaybeUninit::new(*byte);
     }
     out[tag_align] = MaybeUninit::new(1);
-    let out = unsafe { std::mem::transmute_copy::<[MaybeUninit<u8>; SIZE], Enum>(&out) };
+    let out = unsafe { std::mem::transmute_copy::<[MaybeUninit<u8>; SIZE], Enum<u16>>(&out) };
     assert_eq!(out, second);
 }
